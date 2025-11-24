29b05ca55dcb34a1621b5884f1d1ee5c32488c65
prtp <piratapazzo8686@gmail.com>
2025-11-24 18:52:08 +0100

    Renamed to "Opifex", created module system, GUI and docs + minor changes
    Decided "Opifex" as name for the project and, consequently, renamed
    conda env from "aligner" to "opifex" (created small tool,
    "devtools/update-env-file.sh", to quickly update environment.yml). Also,
    created and implemented a logo for Opifex ("source/img/logo.svg" and
    "source/img/logo.png").
    Implemented a modular script system to make expanding this project much
    easier. Consequently, added modules.py and basemodule.py to handle
    modules, moved all content creation scripts to the "modules" folder
    (with proper edits to adapt to the module system), and added
    modules/reddit-video-generator.py as a module, to do what main.py used
    to do earlier.
    Added a full GUI system for single mode execution (as well as introduced
    the concept of execution modes in general) in main.py, using PyQt6. All
    console output (except for external programs called using subprocess,
    with no proper settings) now gets redirected to a visual element in the
    GUI, when in this mode. Other modes are not implemented yet.
    Created README.md and converted it to README.html for easier view,
    containing all informations and documentations about the program,
    basically a small manual. Remember to create a tool, in future, that
    automatically converts README.md to README.html.
    Moved piper files (piper/*) to the source folder (source/piper/*).
    Added more info to requirements.txt, which must be used in the future to
    create a proper installer.
    Modified .gitignore to include VScode configs and cached scripts.
    Added all important variables to config.py, the configuration script.

---
+++ basemodule.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any, List, Tuple
+
+class ModuleResultType:
+    def __init__(self, exception:Exception|None, data:Dict[str, Any]):
+        self.exception = exception
+        self.data = data
+    
+    def __str__(self):
+        return f"ModuleResultType: exception <{str(self.exception)}> data <{str(self.data)}>"
+
+class BaseModule:
+    def __init__(self):
+        self.name = ABC
+        self.description = ABC
+        self.requiredArgs:List[Tuple[str,type]] = ABC #list of required parameters as (name, type) pairs
+        self.returnedDataTypes:List[Tuple[str,type]] = ABC #list of returned key values and respective data types in returned data dict of ModuleResultType
+        self.dependencies:List[str] = ABC #list of "dependencies" (other plugins' name) required for the plugin to work
+
+    def __str__(self):
+        return f"Module <{self.name}>: {self.description.split("\n")[0]}" #prints first line of description, which should always be just the summary. hopefully.
+    
+    @abstractmethod
+    def execute(self, **kwargs) -> ModuleResultType:
+        """Execute the module and return results"""
+        pass
